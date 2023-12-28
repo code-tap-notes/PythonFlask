5adb6619c6c8aed05beaace3f7a3976a87df597b
code-tap-notes <nguyentuongvien@yahoo.com>
2023-12-28 18:48:45 +0100

    Su dung redirect, url_for va truyen ten bien vao url   Python-Flask

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return "Vien"
+    return "<h1> Vien </h1>"
+
+
+@app.route('/admin')
+def hello_admin():
+    return f"<h2> Hello Admin of web ! </h2>"
+
+
+@app.route('/user/<name>')
+def hello_user(name):
+    if name == 'admin':
+        return redirect(url_for('hello_admin'))
+    return f"<h2> Hello {name}! </h2>"
+
 if __name__=="__main__":
-    app.run()
+    app.run(debug=True)
