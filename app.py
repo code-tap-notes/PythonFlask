941e423b638c5d2bca742eabca36226762be1c96
code-tap-notes <nguyentuongvien@yahoo.com>
2023-12-28 19:26:12 +0100

    Su dung template  html ke thua nhau  Python-Flask

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html', name="Vien")
 
+@app.route('/home')
+def homePage():
+    return render_template('home.html') 
 
 @app.route('/admin')
 def hello_admin():
