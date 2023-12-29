c4137c49c6dd18109d25ced2b22b4dae3e266a9d
code-tap-notes <nguyentuongvien@yahoo.com>
2023-12-29 10:25:11 +0100

    Dung session trong thu vien session cua Flask Python-Flash

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, session
 
 app = Flask(__name__)
+app.config["SECRET_KEY"]= "MyKey"
 @app.route('/')
 def hello_world():
     return render_template('index.html', name="Vien")
@@ -12,9 +13,11 @@ def homePage():
 @app.route('/login', methods=["POST","GET"])
 def login():
     if request.method == "POST":
+        
         user_name = request.form["name"]
         if user_name:
-            return redirect(url_for("hello_user",name = user_name))
+            session["user"] = user_name
+            return redirect(url_for("hello_user"))
     return render_template("login.html") 
 
 
@@ -24,11 +27,13 @@ def hello_admin():
     return f"<h2> Hello Admin of web ! </h2>"
 
 
-@app.route('/user/<name>')
-def hello_user(name):
-    if name == 'admin':
-        return redirect(url_for('hello_admin'))
-    return f"<h2> Hello {name}! </h2>"
-
+@app.route('/user')
+def hello_user():
+    if "user" in session:
+        name = session["user"]
+      
+        return f"<h2> Hello {name}! </h2>"
+    else:
+        return redirect(url_for("login"))
 if __name__=="__main__":
     app.run(debug=True)
