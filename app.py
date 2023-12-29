056617d570df7934d842c61c51a0a93a46200da5
code-tap-notes <nguyentuongvien@yahoo.com>
2023-12-29 10:51:04 +0100

    Dung timedelta  trong thu vien datetime  cua Flask Python-Flash

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, redirect, url_for, render_template, request, session
-
+from datetime import timedelta 
 app = Flask(__name__)
 app.config["SECRET_KEY"]= "MyKey"
+app.permanent_session_lifetime = timedelta(minutes=1)
 @app.route('/')
 def hello_world():
     return render_template('index.html', name="Vien")
@@ -15,9 +16,13 @@ def login():
     if request.method == "POST":
         
         user_name = request.form["name"]
+        session.permanent = True
         if user_name:
             session["user"] = user_name
             return redirect(url_for("hello_user"))
+    if "user" in session:
+        name = session["user"]
+        return f"<h2>you are login Hello {name}! </h2>"
     return render_template("login.html") 
 
 
@@ -35,5 +40,9 @@ def hello_user():
         return f"<h2> Hello {name}! </h2>"
     else:
         return redirect(url_for("login"))
+@app.route('/logout')
+def log_out():
+    session.pop("user",None)
+    return redirect(url_for("login"))
 if __name__=="__main__":
     app.run(debug=True)
