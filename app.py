4aa409675f903b9404fc58ab3aad17a3e0099095
code-tap-notes <nguyentuongvien@yahoo.com>
2024-01-02 19:46:56 +0100

    edit, delete du lieu Flask-Python

--- app.py
+++ app.py
@@ -34,16 +34,39 @@ def home():
 @app.route('/home')
 def homePage():
     return render_template("home.html") 
+@app.route('/list')
+def list():
+    users = User.query.order_by(User.username).all()
+    return render_template("list.html",users = users) 
+#@app.route('/users', methods = ['GET'])
+#def get_users():
+ #   all_users = User.query.all()
+   # result = products_shema.dump(all_products)
+    #return jsonify(result)
+@app.route('/delete', methods=["POST","GET"])
+def delete():
+    user_name = request.form["name"]
+    User.query.filter_by(username = user_name).delete()
+    db.session.commit()
+    flash("user deleted")
+    return redirect(url_for("list")) 
 
 @app.route('/login', methods=["POST","GET"])
 def login():
     if request.method == "POST":
-        
+       
         user_name = request.form["name"]
         session.permanent = True
         if user_name:
             session["user"] = user_name
-            flash("Login successfully !","info")
+            found_user = User.query.filter_by(username = user_name).first()
+            if found_user:
+              session["email"] = found_user.email
+            else:   
+                user = User(user_name,"temp@gmail.com")
+                db.session.add(user)
+                db.session.commit()
+            flash("Login successfully ! add to database","info")
         return redirect(url_for("user",user=user_name)) 
     if "user" in session:
         name = session["user"]
@@ -57,12 +80,21 @@ def admin():
     return f"<h2> Hello Admin of web ! </h2>"
 
 
-@app.route('/user')
+@app.route('/user', methods=["POST","GET"])
 def user():
+    email = None
     if "user" in session:
         name = session["user"]
-      
-        render_template("user.html",user=name) 
+        if request.method == "POST":
+            email = request.form["email"]
+            session["email"] = email
+            found_user = User.query.filter_by(username = name).first()
+            found_user.email = email
+            db.session.commit()
+            flash("email updated")
+        elif "email" in session:
+            email = session["email"]
+        return render_template("user.html",user=name, email = email) 
     else:
         flash("You haven't been Logged in !","info")
         return redirect(url_for("login"))
